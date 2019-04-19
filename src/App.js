b5889e552aa14ce3a2d76c02bb820962e0d6f92f
Raj <rraju12@gmail.com>
2019-04-19 11:30:25 +0100

    fix typo in the welcome

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>
-            Wellcome to RajDevDep!
+            Welcome to RajDevDep!
           </h1>
 
         </header>
